d26f05bf78219dfc19ba270ec9f752dfa2e77c84
Sebastien Chapuis <sebastien@chapu.is>
2024-03-30 21:53:21 +0100

    Color based on our position

--- example/rust/gl-glfw/src/main.rs
+++ example/rust/gl-glfw/src/main.rs
@@ -40,19 +40,23 @@ const VERTEX_SHADER: &CStr = c"#version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 out vec3 ourColor;
+out vec3 ourPosition;
 uniform float x_offset;
 void main()
 {
     gl_Position = vec4(aPos.x + x_offset, aPos.y, aPos.z, 1.0);
+    ourPosition = gl_Position.xyz;
     ourColor = aColor;
 }";
 
 const FRAG_SHADER: &CStr = c"#version 330 core
 out vec4 FragColor;
 in vec3 ourColor;
+in vec3 ourPosition;
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = vec4(ourPosition, 1.0f);
+    // FragColor = vec4(ourColor, 1.0f);
     // FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }";
 
